f193643b2b3d0ebf3d8d70f725ecbb00d40c195d
hoangdat3179 <93390662+hoangdat3179@users.noreply.github.com>
2021-11-20 10:58:54 +0700

    lesson 02

---
+++ DateTimeExample/src/vn/techmaster/Main.java
@@ -0,0 +1,75 @@
+package vn.techmaster;
+
+public class Main {
+
+    public static void main(String[] args) {
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.Month;
+import java.time.format.DateTimeFormatter;
+
+        public class Main {
+            public static void main(String[] args) {
+
+                //Lấy ngày hiện tại
+//                LocalDate today = LocalDate.now();
+//                System.out.println(today);
+//
+//                System.out.println("Ngày mai: " + today.plusDays(1));
+//                System.out.println("Tháng sau: " + today.plusMonths(1));
+//                System.out.println("10 Năm sau: " + today.plusYears(10));
+//                System.out.println("Tuần: " + today.plusWeeks(3));
+//
+//                System.out.println("Hôm qua: " + today.minusDays(1));
+//
+//                System.out.println("Lấy ngày hôm nay theo tháng: " + today.getDayOfMonth());
+//                System.out.println("Lây ngày hôm nay theo tuần: " + today.getDayOfWeek());
+//                System.out.println("Lấy ngày trong năm: " + today.getDayOfYear());
+//
+//                System.out.println("Lấy tháng hiện tại: " + today.getMonthValue());
+//                System.out.println("Lấy tháng enum : " + today.getMonth());
+//
+//                System.out.println("Lấy năm hiện tại: " + today.getYear());
+//
+//                //Lấy ngày chỉ định
+//                int year = 2021;
+//                int month = 11;
+//                int dayOfMonth = 12;
+//                LocalDate currenDate = LocalDate.of(year, month, dayOfMonth);
+//                System.out.println("current date: " + currenDate);
+//
+//                LocalDate date = LocalDate.of(2021, Month.JULY, 30);
+//                System.out.println("date: " + date);
+//
+//                LocalTime now = LocalTime.now();
+//                System.out.println("Bây giờ là: " + now);
+//
+//                System.out.println("Sau 1 tiếng nữa: " + now.plusHours(1));
+//                System.out.println("Sau 55 phút nữa: " + now.plusMinutes(70));
+//
+//                System.out.println("Giảm 2 tiếng: " + now.minusHours(2));
+//
+//                System.out.println("Lấy giờ hiện tại: " + now.getHour());
+//                System.out.println("Lấy phút hiện tại: " + now.getMinute());
+//
+//                LocalTime time = LocalTime.of(21, 30, 12);
+//                System.out.println("Thời gian chỉ định: " + time);
+//
+//                LocalDateTime dateTime = LocalDateTime.now();
+//                System.out.println("Ngày và giờ hiện tại: " + dateTime);
+//                LocalDateTime dateTime2 = LocalDateTime.of(date, time);
+//                System.out.println("Ngày và giờ chỉ định: " + dateTime2);
+//
+//                LocalDateTime dateTime3 = LocalDateTime.of(2019, 10, 20, 5, 15);
+//                System.out.println(dateTime3);
+//
+//                DateTimeFormatter myFormat = DateTimeFormatter.ofPattern("dd-MM-yyyy");
+//                System.out.println(date.format(myFormat));
+//
+//                DateTimeFormatter myFormatDateTime = DateTimeFormatter.ofPattern("hh:mm, dd/MM/yyyy");
+//                System.out.println(dateTime3.format(myFormatDateTime));
+
+
+            }
+        }
