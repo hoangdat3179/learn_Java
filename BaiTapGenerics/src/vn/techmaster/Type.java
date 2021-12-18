b1325e95e151e7fe62eb4adbf88c80f3bbec79c2
hoangdat3179 <hoangdat3179@gmail.com>
2021-12-19 01:42:13 +0700

    Generics

--- BaiTapGenerics/src/vn/techmaster/Type.java
+++ BaiTapGenerics/src/vn/techmaster/Type.java
@@ -1,19 +1,5 @@
 package vn.techmaster;
 
-public enum Type implements java.lang.reflect.Type {
-    Waiter("Phục vụ"),
-
-    Kitchen("Đầu bếp");
-
-    private String value;
-
-    Type(String value) {
-
-        this.value = value;
-    }
-    public String getValue() {
-
-        return value;
-    }
-
+public enum Type {
+    Waiter, Kitchen;
 }
